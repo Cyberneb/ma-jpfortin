0f24edc6c80499a13cbe66cc8030f84971d53849
Eric Allatt <eric@lesite.ca>
2014-03-27 17:20:45 -0400

    JF-30

--- app/code/local/Lesite/Erp/Model/Resource/Setup.php
+++ app/code/local/Lesite/Erp/Model/Resource/Setup.php
@@ -1,22 +1,7 @@
 <?php
 
-/* 
- * Brand by Lesite
- * @copyright   Copyright (c) 2014 Lesite http://www.lesite.ca/ *
- */
-
- 
-class Lesite_Brand_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+class Lesite_Erp_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
 {
-    public function startSetup()
-    {
-        $this->getConnection()->startSetup();
-        return $this;
-    }
 
-    public function endSetup()
-    {
-        $this->getConnection()->endSetup();
-        return $this;
-    }
+    
 }
