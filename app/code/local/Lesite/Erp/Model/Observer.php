d055e6ac0c1786665abff486703eed8b65f275d5
Eric Allatt <eric@lesite.ca>
2014-04-03 09:24:28 -0400

    JF-30

---
+++ app/code/local/Lesite/Erp/Model/Observer.php
@@ -0,0 +1,22 @@
+<?php
+
+class Lesite_Erp_Model_Observer
+{
+    public function syncProductViewed($observer)
+    {
+        $event = $observer->getEvent();
+        $product = $event->getProduct();
+        Mage::getResourceModel('lesite_erp/productSync')
+            ->accessProduct($product->getSku());
+        $socket = fsockopen($_SERVER['HTTP_HOST'],80,$errorno,$errorstr,10);
+        if ( $socket )
+        {
+            $socketdata = "GET /erp/test HTTP 1.1\r\nHost: "
+                . $_SERVER['HTTP_HOST'] . "\r\nConnection: Close\r\n\r\n";
+            fwrite($socket, $socketdata);
+            fclose($socket);
+            Mage::log($socketdata, null, 'mylogfile.log');
+        }
+    }
+}
+
